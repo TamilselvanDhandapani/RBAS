f47ff2048849d304ee8ce4a0dd57833771f83990
TamilselvanDhandapani <137706994+TamilselvanDhandapani@users.noreply.github.com>
2025-05-12 19:26:18 +0530

    Updated PersonalInfoForm.jsx

--- client/src/Componets/User/PersonalInfoForm.jsx
+++ client/src/Componets/User/PersonalInfoForm.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../../context/AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import "../../styles/PersonalInfo.css"
+
 
 const PersonalInfoForm = () => {
   const { user } = useAuth();
